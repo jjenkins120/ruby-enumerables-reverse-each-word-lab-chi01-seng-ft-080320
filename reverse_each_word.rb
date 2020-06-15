c03a23577e5d462fd15e8836e8d90982b838ba87
Jeff Jenkins <jjenkins120@gmail.com>
2020-06-15 20:08:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,20 @@
+#def reverse_each_word(string)
+ # string_array = string.split(/ /)
+ # string_array.each {|element| element.reverse}
+  # reverse_string = string_array.to_s
+  # reverse_string
+# end    
+
 def reverse_each_word(string)
-  string_array = string.split(/ /)
-  string_array.each {|element| element.reverse}
-  reverse_string = string_array.to_s
-  reverse_string
-end    
+ original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
+
 
 def reverse_each_word_with_collect(string)
   string_array = string.split()
